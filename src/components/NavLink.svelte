fb8eb0f36bd45b71f5c5e87d8d7c17a9e97fa373
Danny Lin <danny@kdrag0n.dev>
2020-03-22 22:43:05 -0700

    refactor nav

---
+++ src/components/NavLink.svelte
@@ -0,0 +1,38 @@
+<script>
+    export let segment;
+    export let slug;
+    export let link = slug;
+    export let linkSlug = link === slug ? slug : undefined;
+</script>
+
+<style>
+	li {
+		display: block;
+		float: left;
+	}
+
+	[aria-current] {
+		position: relative;
+		display: inline-block;
+	}
+
+	[aria-current]::after {
+		position: absolute;
+		content: '';
+		width: calc(100% - 1em);
+		height: 2px;
+		background-color: rgb(255,62,0);
+		display: block;
+		bottom: -1px;
+	}
+
+	a {
+		text-decoration: none;
+		padding: 1em 0.5em;
+		display: block;
+	}
+</style>
+
+<li>
+    <a rel=prefetch aria-current='{segment === linkSlug ? "page" : undefined}' href={link}>{slug}</a>
+</li>
