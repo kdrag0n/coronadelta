2f153f0824aeef34c1bc645edaea0878371f579d
Danny Lin <danny@kdrag0n.dev>
2020-03-22 21:54:45 -0700

    chart.js

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,31 @@
 <script>
-	export let name;
-</script>
+	import { onMount } from "svelte";
+	import Chart from "chart.js";
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+	export let name;
+
+	onMount(async () => {
+		//const response = await fetch("")
+	});
+	function renderChart() {
+		var ctx = document.getElementById("myChart").getContext("2d");
+		var chart = new Chart(ctx, {
+			type: "line",
+			data: {
+				labels: ["January", "February", "March", "April", "May", "June", "July"],
+				datasets: [
+					{
+						label: "My First dataset",
+						backgroundColor: "rgb(255, 99, 132)",
+						borderColor: "rgb(255, 99, 132)",
+						data: [0, 10, 5, 2, 20, 30, 45]
+					}
+				]
+			},
+			options: {}
+		});
+	}
+</script>
 
 <style>
 	main {
@@ -29,3 +49,9 @@
 	}
 </style>
 
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={renderChart}>Load</button>
+	<canvas id="myChart"></canvas>
+</main>
