f9dc6f4c236b3899cd50541e2bf080149ceba350
Danny Lin <danny@kdrag0n.dev>
2020-03-27 21:45:30 -0700

    PageMeta: Move Twitter embed info above OpenGraph

--- src/components/PageMeta.svelte
+++ src/components/PageMeta.svelte
@@ -11,15 +11,15 @@
     <title>{title} • {site}</title>
     <meta name="description" content={description}>
 
-    <meta name="twitter:card" content="summary">
-    <meta name="twitter:site" content="@corona_delta">
-    <meta name="twitter:creator" content="@kdrag0n">
-    <meta name="twitter:image" content="http://www.example.com/image.jpg">
-
     <meta property="og:title" content="{title}" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://{domain}/{slug}" />
     <meta property="og:image" content="http://example.com/image.jpg" />
     <meta property="og:description" content="Live insights into fresh statistics for the COVID-19 coronavirus pandemic, complete with interactive charts." />
     <meta property="og:site_name" content="{site}" />
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@corona_delta">
+    <meta name="twitter:creator" content="@kdrag0n">
+    <meta name="twitter:image" content="http://www.example.com/image.jpg">
 </svelte:head>
