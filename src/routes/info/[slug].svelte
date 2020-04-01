d430d8db10f9a0a3621f1bc6e417452dfdd981ab
Danny Lin <danny@kdrag0n.dev>
2020-03-31 22:46:40 -0700

    info: Extract PageMeta description from front matter

--- src/routes/info/[slug].svelte
+++ src/routes/info/[slug].svelte
@@ -1,3 +1,7 @@
+<PageMeta title={page.title}
+          description={page.description}
+          slug={page.slug} />
+
 <script context="module">
     export async function preload({ params, query }) {
         // the `slug` parameter is available because
@@ -14,18 +18,12 @@
 </script>
 
 <script>
+    import PageMeta from "../../components/PageMeta.svelte";
+
     export let page;
 </script>
 
 <style>
-    /*
-        By default, CSS is locally scoped to the component,
-        and any unused styles are dead-code-eliminated.
-        In this page, Svelte can't know which elements are
-        going to appear inside the {{{page.html}}} block,
-        so we have to use the :global(...) modifier to target
-        all elements inside .content
-    */
     .content :global(h2) {
         font-size: 1.4em;
         font-weight: 500;
@@ -52,10 +50,6 @@
         margin: 0 0 0.5em 0;
     }
 </style>
-
-<svelte:head>
-    <title>{page.title}</title>
-</svelte:head>
 
 <h1>{page.title}</h1>
 
--- src/routes/info/pages/test.md
+++ src/routes/info/pages/test.md
@@ -1,5 +1,6 @@
 ---
 title: "Test page"
+description: "A simple test page."
 ---
 
 This is a test page.
