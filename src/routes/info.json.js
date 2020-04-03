ee247957c1a9c45d6837d1898fa7bbcd1a5ce732
Danny Lin <danny@kdrag0n.dev>
2020-04-02 23:45:24 -0700

    info: Allow disabling indexing on a per-page basis

--- src/routes/info.json.js
+++ src/routes/info.json.js
@@ -15,11 +15,14 @@ async function getPages() {
         const post = await fs.readFile(path.resolve(pageDir, fileName), "utf-8");
         const slug = fileName.replace(/\.md$/, "");
         const frontMatter = grayMatter(post);
-        
-        pages.push({
-            slug,
-            ...frontMatter.data
-        });
+
+        // default true
+        if (frontMatter.data.index !== false) {
+            pages.push({
+                slug,
+                ...frontMatter.data
+            });
+        }
     }
 
     return pages;
